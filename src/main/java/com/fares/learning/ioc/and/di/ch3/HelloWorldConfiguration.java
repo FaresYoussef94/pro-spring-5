74f22faf4d5f177144424c9c9c0e5955c978f47e
Fares Youssef <fares.youssef@seal-software.com>
2019-03-25 23:03:44 +0200

    adding the bean configuration using java classes

---
+++ src/main/java/com/fares/learning/ioc/and/di/ch3/HelloWorldConfiguration.java
@@ -0,0 +1,10 @@
+package com.fares.learning.ioc.and.di.ch3;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = {"com.fares.learning.ioc.and.di.ch3"})
+@Configuration
+public class HelloWorldConfiguration {
+
+}
