69f74385e7db2a6a56e974593cbd449dfcf7e189
Fares Youssef <fares.youssef@seal-software.com>
2019-03-28 01:42:17 +0200

    adding method lookup

---
+++ src/main/java/com/fares/learning/ioc/and/di/ch3/Singer.java
@@ -0,0 +1,12 @@
+package com.fares.learning.ioc.and.di.ch3;
+
+public class Singer {
+
+	private String lyric = "I played a quick game of chess with the salt and pepper shaker";
+
+	public void sing() {
+		// commented because it pollutes the output
+		// System.out.println(lyric);
+	}
+
+}
