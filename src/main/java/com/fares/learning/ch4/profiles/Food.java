35361e403c0c899db831e208eb55ce95bcf597b5
Fares Youssef <fares.youssef@seal-software.com>
2019-04-27 22:42:18 +0200

    adding profiles

---
+++ src/main/java/com/fares/learning/ch4/profiles/Food.java
@@ -0,0 +1,23 @@
+package com.fares.learning.ch4.profiles;
+
+public class Food {
+
+	private String name;
+
+	public Food() {
+
+	}
+	
+	public Food(String name) {
+		this.name=name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/com/fares/learning/ch4/profiles/FoodProviderService.java
@@ -0,0 +1,9 @@
+package com.fares.learning.ch4.profiles;
+
+import java.util.List;
+
+public interface FoodProviderService {
+	
+	List<Food> provideLunchSet();
+	
+}
